11754b24eba1b2922a67e0eca09d8a7c6672e9d8
Furkan Emre Saygın <furkanemresaygin@gmail.com>
2025-10-24 18:32:51 +0300

    Update categories.js

--- Api/routes/categories.js
+++ Api/routes/categories.js
@@ -20,4 +20,25 @@ router.get('/', function(req, res, next) {
 });
 
 
+router.post('/update', async(req, res, next)=>{
+  try{
+    if (!req.body.id) {
+      throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, "Validation Error", 'Id Faild is required');
+    }
+    if(body.name){
+      let updates = {};
+      updates.name = body.name;
+    }
+    if(body.is_active === typeof Boolean){ 
+      updates.is_active = body.is_active;
+    }
+    await Categories.updates({_id: body.id}, updates);
+    res.json(Response.successResponse({success: true}));
+    
+  }catch(err){
+      let errorResponse = Response.errorResponse(err);
+      res.status(errorResponse.code).json(errorResponse);
+  } 
+
+})
 module.exports = router;
