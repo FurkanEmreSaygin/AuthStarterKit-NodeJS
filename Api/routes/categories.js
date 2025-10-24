90397c1348c9e7dcd06e0f4d5084ef77645ce09b
Furkan Emre Saygın <furkanemresaygin@gmail.com>
2025-10-24 10:03:51 +0300

    Erros Specification
    Made a custom error files.

--- Api/routes/categories.js
+++ Api/routes/categories.js
@@ -1,23 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const Categories = require('../db/models/Categories');
+const Response = require('../lib/Response');
+const CustomError = require('../lib/Error');
+const Enum = require('../config/Enum');
 
+router.get('/', async(req, res, next)=>{
+  try {
+    let categories = await Categories.find({});
+    res.json(Response.successResponse(categories));
 
-router.get('/:id', function(req, res, next) {
-  res.json({
-    message: 'Categories Endpoint',
-    body: req.body,
-    params: req.params,
-    query: req.query,
-    Headers: req.headers
+  } catch (err) {
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(Response.errorResponse(err));
+
   }
-  );
-  next();
 });
 
+router.post('/add', async(req, res, next)=>{
+  let body = req.body;
+  try {
+    if (!body.name) {
+      throw new CustomError(Enum.HTTP_CODES.BAD_REQUEST, "Validation Error", 'Category name is required');
+    };
 
-router.get('/', function(req, res, next) {
-  res.send('Categories Endpoint');
+    let newCategory = new Categories({
+      is_active: body.is_active,
+      name: body.name,
+      createdBy: req.user?.id
+    });
+
+    await newCategory.save();
+    res.json(Response.successResponse({success: true}));
+
+  } catch (err) {
+
+    let errorResponse = Response.errorResponse(err);
+    res.status(errorResponse.code).json(errorResponse);
+  
+  }
 });
-
 
 module.exports = router;
